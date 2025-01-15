79d367f0bfcb6fa10b8c4af043a903d38f926f8a
NarciSource <jwch11@gmail.com>
2025-01-16 00:36:31 +0900

    design: 채팅 UI 디자인 수정

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="q-pa-md row justify-center">
-    <div class="full-width" style="max-width: 400px">
-      <div class="text-h6">Chat</div>
-      <dv>
-        <q-chat-message :text="[text]" sent />
-        <q-chat-message :text="[reply]" />
-      </dv>
+    <q-card dark bordered class="card">
+      <q-card-section class="text-h6">채팅 창</q-card-section>
+      <q-separator dark inset />
 
-      <div class="row items-end justify-end q-gutter-md">
-        <q-input class="w-200" v-model="text" label="Message" />
-        <q-btn class="q-mt-md float-right" label="Send" @click="send" />
-      </div>
-    </div>
+      <q-card-section>
+        <q-chat-message :text="[send_message]" sent bg-color="yellow" />
+        <q-chat-message :text="[reply_message]" bg-color="white" />
+      </q-card-section>
+
+      <q-card-section class="row items-end justify-end no-margin q-gutter-x-md bg-white">
+        <q-input class="w-200" v-model="send_message" label="Message" @keyup.enter="send" />
+        <q-btn
+          class="q-mt-md float-right"
+          color="yellow"
+          text-color="black"
+          label="Send"
+          @click="send"
+        />
+      </q-card-section>
+    </q-card>
   </div>
 </template>
 
@@ -23,16 +31,23 @@ const SOCKET_SERVER_URL = import.meta.env.VITE_SOCKET_SERVER_URL;
 const SOCKET_EVENT_MESSAGE = import.meta.env.VITE_SOCKET_EVENT_MESSAGE;
 const SOCKET_EVENT_RESPONSE = import.meta.env.VITE_SOCKET_EVENT_RESPONSE;
 
-const text = ref<string>("");
-const reply = ref<string>("");
+const send_message = ref<string>("입력해주세요");
+const reply_message = ref<string>("응답 대기");
 
 const socket: Socket = io(SOCKET_SERVER_URL, {
   transports: ["websocket"],
 });
 
 socket.on(SOCKET_EVENT_RESPONSE, (message: string) => {
-  reply.value = message;
+  reply_message.value = message;
 });
 
-const send = () => socket.emit(SOCKET_EVENT_MESSAGE, text.value);
+const send = () => socket.emit(SOCKET_EVENT_MESSAGE, send_message.value);
 </script>
+
+<style scoped>
+.card {
+  background-color: #464647;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+}
+</style>
