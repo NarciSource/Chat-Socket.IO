9bed0e39aee4f466b8e586359a3f5161529ade9c
NarciSource <jwch11@gmail.com>
2025-01-21 00:03:48 +0900

    feat: 일대일 연결 기능 추가

---
+++ src/features/room/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-input v-model="my_nick" label="내 닉네임" />
+  <q-input v-model="opponent_nick" label="상대방 닉네임" />
+  <q-btn label="연결" color="yellow" text-color="black" @click="create_room" />
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+
+import { useRoomStore } from "./store/room";
+import { register } from "@/entities/service/socketService";
+
+const { my_nick, opponent_nick } = storeToRefs(useRoomStore());
+
+const create_room = () => {
+  register(my_nick.value);
+};
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,9 @@
 <template>
+  <Room />
   <Chat />
 </template>
 
 <script setup>
+import Room from "@/features/room";
 import Chat from "@/features/chat";
 </script>
