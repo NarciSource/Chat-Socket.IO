9d665662499ca6f5cc7f05c4df9207bd2da1b748
NarciSource <jwch11@gmail.com>
2025-01-22 23:10:55 +0900

    refactor: 전체 메시지 목록과 현재 메시지 목록 구분

--- src/features/chat/store/chat.ts
+++ src/features/chat/store/chat.ts
@@ -1,4 +1,4 @@
-import { reactive, ref } from "vue";
+import { computed, reactive, ref } from "vue";
 import { defineStore } from "pinia";
 
 import Message from "@/entities/chat/model/Message";
@@ -7,25 +7,27 @@ export const useChatStore = defineStore("chat", () => {
   const connecting = ref(false); // 소켓 연결 여부
   const room_id = ref<string>("");
   const my_nick = ref<string>("");
-  const messages = reactive<{ [key: string]: Message[] }>({}); // 전체 메시지 목록
   const query = ref(""); // 검색어
   const searching = ref(false); // 검색 중 여부
 
+  const message_dictionary = reactive<{ [key: string]: Message[] }>({}); // 전체 메시지 목록
+  const messages = // 현재방 메시지 목록
+    computed(() => {
+      // 방에 메시지가 없으면 빈 배열로 초기화
+      message_dictionary[room_id.value] = message_dictionary[room_id.value] || [];
+      return message_dictionary[room_id.value];
+    });
+
   // 메시지 삽입 함수
   const insert_message = (message: Message) => {
-    // 방에 메시지가 없으면 빈 배열로 초기화
-    if (!messages[room_id.value]) {
-      messages[room_id.value] = [];
-    }
-
     // 마지막 메시지 인덱스
-    const last_index = messages[room_id.value].length - 1;
+    const last_index = message_dictionary[room_id.value].length - 1;
 
     // 마지막 메시지와 동일한 대상이 보낸 메시지인 경우 이어서 추가
-    if (messages[room_id.value][last_index]?.name === message.name && !message.is_system) {
-      messages[room_id.value][last_index].add_text(message.text[0]);
+    if (messages.value[last_index]?.name === message.name && !message.is_system) {
+      messages.value[last_index].add_text(message.text[0]);
     } else {
-      messages[room_id.value].push(message);
+      messages.value.push(message);
     }
   };
 
--- src/features/chat/ui/chat-content.vue
+++ src/features/chat/ui/chat-content.vue
@@ -5,21 +5,22 @@
     :name="!message.is_system ? message.name : ''"
     :label="message.is_system ? message.text[0] : ''"
     :text="!message.is_system ? message.text : []"
-    :sent="message.name === store.my_nick"
-    :bg-color="message.name === store.my_nick ? 'yellow' : 'white'"
+    :sent="message.name === my_nick"
+    :bg-color="message.name === my_nick ? 'yellow' : 'white'"
   />
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
+import { storeToRefs } from "pinia";
 
 import { useChatStore } from "../store/chat";
 
-const store = useChatStore();
+const { my_nick, messages, query } = storeToRefs(useChatStore());
 
 // 검색 결과 필터링
 const filtered_messages = computed(() => {
-  return (store.messages[store.room_id] || []).filter((message) => message.include(store.query));
+  return messages.value.filter((message) => message.include(query.value));
 });
 </script>
 
