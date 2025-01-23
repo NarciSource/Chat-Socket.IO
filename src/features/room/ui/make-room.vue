4c023bed6e34fbb30fedfe4e599c9266dd8f0f48
NarciSource <jwch11@gmail.com>
2025-01-23 22:30:24 +0900

    feat: 방 만들시 사용자 선택 기능 추가

--- src/features/room/ui/make-room.vue
+++ src/features/room/ui/make-room.vue
@@ -1,10 +1,10 @@
 <template>
   <q-btn class="q-ma-md full-width" label="방 만들기" color="teal" text-color="white">
     <q-popup-proxy>
-      <user-list />
+      <user-list v-model="selected_users" />
 
       <div class="q-ma-md row justify-end q-gutter-sm">
-        <q-btn label="생성" color="teal" />
+        <q-btn label="생성" color="teal" @click="make" />
         <q-btn label="취소" />
       </div>
     </q-popup-proxy>
@@ -12,5 +12,13 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+
 import UserList from "./user-list.vue";
+
+const selected_users = ref<string[]>([]);
+
+const make = () => {
+  console.log(selected_users.value);
+};
 </script>
--- src/features/room/ui/user-list.vue
+++ src/features/room/ui/user-list.vue
@@ -1,43 +1,40 @@
 <template>
-  <q-list>
-    <q-item v-for="(user, index) in users" clickable v-ripple @click="() => join(user)">
-      <q-item-section avatar>
-        <q-avatar class="overlapping">
-          <img :src="`https://cdn.quasar.dev/img/avatar${index + 1}.jpg`" />
-        </q-avatar>
-      </q-item-section>
+  <q-item tag="label" v-for="(user, id) in users" :active="checks[user]" active-class="bg-teal-2">
+    <q-item-section side v-show="false">
+      <q-checkbox v-model="checks[user]" />
+    </q-item-section>
 
-      <q-item-section>
-        <q-item-label>{{ user }}</q-item-label>
-      </q-item-section>
+    <q-item-section avatar>
+      <q-avatar>
+        <img :src="`https://cdn.quasar.dev/img/avatar${id + 1}.jpg`" />
+      </q-avatar>
+    </q-item-section>
 
-      <q-item-section side>
-        <q-badge rounded color="green" />
-      </q-item-section>
-    </q-item>
-  </q-list>
+    <q-item-section>
+      <q-item-label>{{ user }}</q-item-label>
+    </q-item-section>
+
+    <q-item-section side>
+      <q-badge rounded color="green" />
+    </q-item-section>
+  </q-item>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import { storeToRefs } from "pinia";
+import { ref, watch } from "vue";
 
-import { join_room } from "@/entities/chat/service/socketService";
-import { useRoomStore } from "../store/room";
-
-const { my_nick, room_id, connecting } = storeToRefs(useRoomStore());
 const users = ref(["user1", "user2", "user3"]);
+const checks = ref(Object.fromEntries(users.value.map((user) => [user, false])));
+const selected_users = defineModel<string[]>({ required: true });
 
-// 방 생성
-const join = (opponent_nick: string) => {
-  if (!connecting.value) {
-    alert("연결을 먼저 해주세요");
-    return;
-  }
-  // 일대일 채팅으로 상대방 이름을 방 이름으로 설정
-  room_id.value = opponent_nick;
-
-  // 방 생성
-  join_room(my_nick.value, room_id.value);
-};
+// 체크박스 상태에 따라 선택된 사용자 목록 업데이트
+watch(
+  () => checks.value,
+  (checks) => {
+    selected_users.value = Object.entries(checks)
+      .filter(([, checked]) => checked)
+      .map(([user]) => user);
+  },
+  { deep: true },
+);
 </script>
