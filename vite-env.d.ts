a691fed8eb4cb984a2862dc6f3ad54728df370b6
NarciSource <jwch11@gmail.com>
2025-01-26 08:30:53 +0900

    feat: 타이핑 관련 환경변수 추가

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -8,11 +8,13 @@ interface ImportMetaEnv {
   readonly VITE_SOCKET_ON_SYSTEM: string;
   readonly VITE_SOCKET_ON_MESSAGE: string;
   readonly VITE_SOCKET_ON_ROOM_CREATED: string;
+  readonly VITE_SOCKET_ON_TYPING: string;
   readonly VITE_SOCKET_EMIT_REGISTER: string;
   readonly VITE_SOCKET_EMIT_MESSAGE: string;
   readonly VITE_SOCKET_EMIT_CREATE_ROOM: string;
   readonly VITE_SOCKET_EMIT_LEAVE_ROOM: string;
   readonly VITE_SOCKET_EMIT_INVITE_ROOM: string;
+  readonly VITE_SOCKET_EMIT_TYPING: string;
   readonly VITE_AUTH_PARCEL_URL: string;
 }
 
