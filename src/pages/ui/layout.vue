094dad6666b9db521393274eea1a91c00182813f
NarciSource <jwch11@gmail.com>
2025-01-27 19:31:47 +0900

    design: 배경화면에 글자 추가

--- src/pages/ui/layout.vue
+++ src/pages/ui/layout.vue
@@ -2,7 +2,7 @@
   <q-layout
     view="lHh LpR lFf"
     container
-    class="q-layout-background shadow-2 rounded-borders"
+    class="q-layout-background shadow-2 rounded-borders background-with-text"
     :style="style"
   >
     <slot name="room" />
@@ -21,3 +21,22 @@ const style = {
   backgroundSize: "cover",
 };
 </script>
+
+<style scoped>
+.background-with-text::before {
+  content: "Chat\AService";
+  white-space: pre; /* 줄 바꿈 처리 */
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%); /* 정확한 중앙 정렬 */
+
+  color: gold;
+  font-family: "Cinzel", serif; /* 폰트 설정 */
+  font-size: 30px;
+  text-align: center;
+
+  text-shadow: 2px 2px 6px #000;
+}
+</style>
