73f951e767bd92cb460eab6473a93274ca109856
NarciSource <jwch11@gmail.com>
2025-01-31 17:43:04 +0900

    feat: FAB 버튼 방향 자동 조정 기능 추가

--- src/pages/chat/ui/fab-layout.vue
+++ src/pages/chat/ui/fab-layout.vue
@@ -3,7 +3,7 @@
     <q-fab
       fab
       icon="chatbubbles-outline"
-      direction="up"
+      :direction="fab_direction"
       color="teal"
       v-touch-pan.prevent.mouse="move_fab"
       @click="(action = !action), (render = true)"
@@ -14,17 +14,40 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 const action = defineModel<boolean>({ required: true });
 const render = ref(false);
+const fab_direction = ref<"up" | "down" | "left" | "right">("up");
 
 // FAB버튼 드래그 기능
-const fab_position = ref([18, 18]);
+const fab_position = ref([18, 18]); // 우하단 기준
 const move_fab = ({ delta }: { delta?: { x?: number; y?: number } }) => {
   fab_position.value = [
     fab_position.value[0] - (delta?.x ?? 0),
     fab_position.value[1] - (delta?.y ?? 0),
   ];
 };
+
+// FAB 컨텐츠의 방향 설정
+const change_direction = () => {
+  const screenWidth = window.innerWidth;
+  const screenHeight = window.innerHeight;
+  const [x, y] = fab_position.value;
+
+  if (x < screenWidth * (1 / 4)) {
+    fab_direction.value = "left";
+  }
+  if (x > screenWidth * (3 / 4)) {
+    fab_direction.value = "right";
+  }
+  if (y < screenHeight * (1 / 4)) {
+    fab_direction.value = "up";
+  }
+  if (y > screenHeight * (3 / 4)) {
+    fab_direction.value = "down";
+  }
+};
+
+watch(fab_position, change_direction);
 </script>
