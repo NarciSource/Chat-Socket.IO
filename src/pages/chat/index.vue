439b3eca35d32e8f2de3d4cc603028b93ae0bf27
NarciSource <jwch11@gmail.com>
2025-01-31 13:34:42 +0900

    feat: 채팅윈도우 토글 구현

--- src/pages/chat/index.vue
+++ src/pages/chat/index.vue
@@ -1,37 +1,26 @@
 <template>
-  <div class="row">
-    <register />
-  </div>
+  <q-layout class="full-height">
+    <q-page-container>
+      <q-page padding>
+        <div class="row">
+          <register />
+        </div>
 
-  <div class="q-pa-md row justify-center">
-    <layout :width="LAYOUT_WIDTH" :height="LAYOUT_HEIGHT">
-      <template #room>
-        <room />
-      </template>
+        <chat-window v-show="show_chat" />
 
-      <template #chat>
-        <chat
-          v-if="!!selected_room?.id"
-          :connecting="connecting"
-          :room="selected_room"
-          :current_user="current_user"
-        />
-      </template>
-    </layout>
-  </div>
+        <q-page-sticky position="bottom-right" :offset="[18, 18]">
+          <q-btn fab icon="chatbubbles-outline" color="teal" @click="show_chat = !show_chat" />
+        </q-page-sticky>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script setup>
-import { storeToRefs } from "pinia";
+import { ref } from "vue";
 
-import Room, { Register, useRoomStore } from "@/features/room";
-import Chat from "@/features/chat";
-import Layout from "./ui/layout.vue";
+import { Register } from "@/features/room";
+import { ChatWindow } from "./ui";
 
-// 레이아웃 크기
-const LAYOUT_WIDTH = import.meta.env.VITE_LAYOUT_WIDTH || 500;
-const LAYOUT_HEIGHT = import.meta.env.VITE_LAYOUT_HEIGHT || 700;
-
-// feature간 데이터 공유
-const { connecting, selected_room, current_user } = storeToRefs(useRoomStore());
+const show_chat = ref(false);
 </script>
---
+++ src/pages/chat/ui/chat-window.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="q-pa-md row justify-center">
+      <layout :width="LAYOUT_WIDTH" :height="LAYOUT_HEIGHT">
+        <template #room>
+          <room />
+        </template>
+  
+        <template #chat>
+          <chat
+            v-if="!!selected_room?.id"
+            :connecting="connecting"
+            :room="selected_room"
+            :current_user="current_user"
+          />
+        </template>
+      </layout>
+    </div>
+  </template>
+  
+  <script setup>
+  import { storeToRefs } from "pinia";
+  
+  import Room, { useRoomStore } from "@/features/room";
+  import Chat from "@/features/chat";
+  import Layout from "./layout.vue";
+  
+  // 레이아웃 크기
+  const LAYOUT_WIDTH = import.meta.env.VITE_LAYOUT_WIDTH || 500;
+  const LAYOUT_HEIGHT = import.meta.env.VITE_LAYOUT_HEIGHT || 700;
+  
+  // feature간 데이터 공유
+  const { connecting, selected_room, current_user } = storeToRefs(useRoomStore());
+  </script>
+  
