1dd6f9c366942ab5cd9ddf1f4f7b4a052b0e6956
NarciSource <jwch11@gmail.com>
2025-02-25 21:13:52 +0900

    feat: 사용자 목록에 참여자 수 표시 추가

--- src/features/users/ui/user-list.vue
+++ src/features/users/ui/user-list.vue
@@ -1,41 +1,49 @@
 <template>
-  <q-item
-    tag="label"
-    v-for="[user, status] in checks_map"
-    :active="status.check"
-    active-class="bg-teal-2"
-  >
-    <q-item-section side v-show="false">
-      <q-checkbox v-model="status.check" />
-    </q-item-section>
+  <q-list>
+    <q-item class="q-pa-md text-subtitle2 text-weight-bold bg-teal-1">
+      참여자 {{ number_of_users }} 명
+    </q-item>
 
-    <q-item-section avatar>
-      <q-avatar>
-        <img :src="user.avatar_url" />
-      </q-avatar>
-    </q-item-section>
+    <q-item
+      tag="label"
+      v-for="[user, status] in checks_map"
+      :active="status.check"
+      active-class="bg-teal-2"
+    >
+      <q-item-section side v-show="false">
+        <q-checkbox v-model="status.check" />
+      </q-item-section>
 
-    <q-item-section>
-      <q-item-label>{{ user.name }}</q-item-label>
-    </q-item-section>
+      <q-item-section avatar>
+        <q-avatar>
+          <img :src="user.avatar_url" />
+        </q-avatar>
+      </q-item-section>
 
-    <q-item-section side>
-      <q-badge rounded color="green" />
-    </q-item-section>
-  </q-item>
+      <q-item-section>
+        <q-item-label>{{ user.name }}</q-item-label>
+      </q-item-section>
+
+      <q-item-section side>
+        <q-badge rounded color="green" />
+      </q-item-section>
+    </q-item>
+  </q-list>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { computed, ref, watch } from "vue";
+import { storeToRefs } from "pinia";
 
 import { User } from "@/entities/chat/model";
 import useUsersStore from "../store/useUsersStore";
 
 const selected_users = defineModel<User[]>({ required: true });
-const { users } = useUsersStore();
+const { users } = storeToRefs(useUsersStore());
+const number_of_users = computed(() => users.value.length);
 // 사용자 목록에 체크박스 바인딩
 const checks_map = ref(
-  new Map<User, { check: boolean }>(users.map((user) => [user, { check: false }])),
+  new Map<User, { check: boolean }>(users.value.map((user) => [user, { check: false }])),
 );
 
 // 체크된 사용자를 선택된 사용자로 업데이트
