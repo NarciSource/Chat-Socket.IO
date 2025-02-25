3cafca1d94dd56f0d4c1ee7a0415ec42e7989d31
yxxngjxn <yxxngjxn99@gmail.com>
2025-02-25 16:23:03 +0900

    fix: chat.module에 chat.gateway 등록 (웹 소켓 연결 오류 해결)

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 import { ChatRepository } from '../chat.repository';
 
 @Module({
-    providers: [ChatService, ChatRepository]
+    providers: [ChatGateway, ChatService, ChatRepository]
 })
 export class ChatModule {}
