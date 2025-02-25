8872210311d8befd04e4944fde4818b28282984c
yxxngjxn <yxxngjxn99@gmail.com>
2025-02-25 15:27:53 +0900

    refactor: REST API 엔드포인트명 변경 및 불필요한 파일 삭제

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('connection')
+  @Get('users')
   getAllConnections() {
     // Service를 통해 데이터 조회
     return this.appService.getAllConnections();
