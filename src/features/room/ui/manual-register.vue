037ccb4574117fabea09bfa8efbae74a69758d76
NarciSource <jwch11@gmail.com>
2025-04-02 18:11:57 +0900

    refactor: manual-register 컴포넌트 분리

---
+++ src/features/room/ui/manual-register.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-dialog v-model="dialog_visible" persistent>
+    <q-card>
+      <q-card-section class="text-h6">수동 등록</q-card-section>
+      <q-card-section>
+        <q-input v-model="my_nick" label="내 닉네임" />
+        <q-btn
+          class="q-ma-md full-width"
+          label="연결"
+          color="teal"
+          text-color="white"
+          @click="click"
+        />
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const { setup } = defineProps<{ setup: (my_nick: string) => void }>();
+const my_nick = ref("");
+const dialog_visible = ref(true);
+
+const click = () => {
+  setup(my_nick.value);
+  dialog_visible.value = false;
+};
+</script>
--- src/features/room/ui/register.vue
+++ src/features/room/ui/register.vue
@@ -1,57 +1,37 @@
 <template>
-  <q-dialog v-model="dialog_visible" persistent>
-    <q-card v-if="MANUAL_USER_SET">
-      <q-card-section class="text-h6">수동 등록</q-card-section>
-      <q-card-section>
-        <q-input v-model="my_nick" label="내 닉네임" />
-        <q-btn
-          class="q-ma-md full-width"
-          label="연결"
-          color="teal"
-          text-color="white"
-          @click="setup"
-        />
-      </q-card-section>
-    </q-card>
-  </q-dialog>
+  <manual-register v-if="MANUAL_USER_SET" :setup="setup" />
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
 import { storeToRefs } from "pinia";
 
 import getUser from "@/shared/lib/getUser";
 import { User } from "@/entities/chat/model";
 import { connect } from "@/entities/chat/service/socketService";
 import useRoomStore from "../store/useRoomStore";
+import manualRegister from "./manual-register.vue";
 
 const MANUAL_USER_SET = import.meta.env.VITE_MANUAL_USER_SET === "true" || false;
 
 const { current_user, connecting } = storeToRefs(useRoomStore());
-const my_nick = ref("");
-const dialog_visible = ref(true);
 
-const setup = () => {
+const setup = (my_nick: string) => {
   // 소켓 연결
   const { register, success } = connect();
 
   // 사용자 정보 생성
-  current_user.value = new User(my_nick.value);
+  current_user.value = new User(my_nick);
 
   // 서버에 사용자 등록
   register(current_user.value.name);
 
   // 연결 성공시 콜백
-  success(() => {
-    connecting.value = true;
-    dialog_visible.value = false;
-  });
+  success(() => (connecting.value = true));
 };
 
 if (!MANUAL_USER_SET) {
+  // auth-parcel을 통해 사용자 정보 가져오기
   const { username } = getUser();
-  my_nick.value = username;
-
-  setup();
+  setup(username);
 }
 </script>
