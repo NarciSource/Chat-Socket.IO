0726d5d53d51efea19604e3eceff452390f42f1c
NarciSource <jwch11@gmail.com>
2025-04-03 11:01:56 +0900

    feat: 레포지토리 구현체 제공을 위한 팩토리 함수

--- src/chat/repository.module.ts
+++ src/chat/repository.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import { RedisModule } from './redis.module';
 import { RedisRepository } from './repository.redis';
@@ -9,7 +10,18 @@ import { SimpleRepository } from './repository.simple';
   providers: [
     {
       provide: 'IRepository', // 추상 레포지토리
-      useClass: RedisRepository, // 구현체
+      useFactory: (ConfigService: ConfigService) => {
+        // 구현체를 선택하는 팩토리 함수
+        const repositoryType = ConfigService.get<string>('REPOSITORY_TYPE', 'simple');
+
+        switch (repositoryType) {
+          case 'simple':
+            return new SimpleRepository();
+          case 'redis':
+            return new RedisRepository(ConfigService.get('REDIS_CLIENT'));
+        }
+      },
+      inject: [ConfigService],
     },
   ],
   exports: ['IRepository'], // 다른 모듈에서는 인터페이스를 사용하여 의존성 주입을 받을 수 있도록 export
