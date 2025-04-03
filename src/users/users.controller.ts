2edc3e427bf04994da397459b07d496d9574e0f1
NarciSource <jwch11@gmail.com>
2025-04-03 13:46:33 +0900

    feat: 헬스 체크 컨트롤러 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 
@@ -10,5 +10,14 @@ export class UsersController {
   getUsers() {
     // Service를 통해 데이터 조회
     return this.appService.getUsers();
+  }
+}
+
+@Controller('/')
+export class HealthCheckController {
+  @Get()
+  @HttpCode(200)
+  async healthCheck() {
+    return 'OK';
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { RepositoryModule } from '../repository/repository.module';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { HealthCheckController, UsersController } from './users.controller';
 
 @Module({
   imports: [RepositoryModule],
-  controllers: [UsersController],
+  controllers: [UsersController, HealthCheckController],
   providers: [UsersService],
 })
 export class UsersModule {}
