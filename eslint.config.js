a3a6864da2bedc302b4e7ec5bcf3c1cf9e95f357
NarciSource <jwch11@gmail.com>
2025-08-04 18:59:33 +0900

    chore: fsd 임포트 레이어 규칙 추가

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,15 @@ import eslint from "@eslint/js";
 import typescriptEslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginVue from "eslint-plugin-vue";
+import importPlugin from "eslint-plugin-import";
 
 export default typescriptEslint.config(
-  { ignores: ["*.d.ts", "**/coverage", "**/dist"] },
+  {
+    ignores: ["*.d.ts", "**/coverage", "**/dist"],
+  },
 
   {
+    plugins: { import: importPlugin },
     extends: [
       eslint.configs.recommended,
       ...typescriptEslint.configs.recommended,
@@ -27,6 +31,47 @@ export default typescriptEslint.config(
       "vue/max-attributes-per-line": ["error", { singleline: 3 }],
       "vue/multi-word-component-names": "off",
       "vue/prop-name-casing": "off",
+
+      // import/order 규칙
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+          pathGroups: [
+            {
+              pattern: "@/app/**",
+              group: "internal",
+              position: "before",
+            },
+            {
+              pattern: "@/pages/**",
+              group: "internal",
+              position: "before",
+            },
+            {
+              pattern: "@/widgets/**",
+              group: "internal",
+              position: "before",
+            },
+            {
+              pattern: "@/features/**",
+              group: "internal",
+              position: "before",
+            },
+            {
+              pattern: "@/entities/**",
+              group: "internal",
+              position: "before",
+            },
+            {
+              pattern: "@/shared/**",
+              group: "internal",
+              position: "before",
+            },
+          ],
+          warnOnUnassignedImports: true,
+        },
+      ],
     },
   },
   eslintConfigPrettier,
