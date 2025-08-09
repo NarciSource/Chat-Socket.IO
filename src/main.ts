9e02c4313aa30865257e593fc77503ff506bdce0
NarciSource <jwch11@gmail.com>
2025-08-09 12:38:35 +0900

    docs: 로그 및 주석 추가

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,15 @@ import { CoreModule } from './core/module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(CoreModule, new ExpressAdapter());
 
-  const adapterConstructor = app.get<ReturnType<typeof createAdapter>>('REDIS_PUBSUB');
+  // RedisIoAdapter(WS)를 사용하여 WebSocket 어댑터 설정
+  const adapterConstructor = app.get<ReturnType<typeof createAdapter>>('REDIS_PUBSUB_ADAPTER');
   const redisIoAdapter = new RedisIoAdapter(app, adapterConstructor);
-  app.useWebSocketAdapter(redisIoAdapter);
+  app.useWebSocketAdapter(redisIoAdapter); // 어댑터 적용
 
+  // CORS 설정 (필요에 따라 조정)
   app.enableCors();
 
+  // 애플리케이션 외부 포트에서 수신 대기
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
