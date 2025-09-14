3ceac30ab795e275127210f1bdbb9ea91bc602bf
NarciSource <jwch11@gmail.com>
2025-09-14 20:12:37 +0900

    refactor: redis 폴더 정리

--- src/common/redis/module.ts
+++ src/common/redis/module.ts
@@ -1,12 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 
-import dbProvider from './db.provider';
-import pubsubProvider from './pubsubAdapter.provider';
-import streamsAdapterProvider from './streamsAdapter.provider';
+import { dbProvider, pubsubAdapterProvider, streamsAdapterProvider } from './providers';
 
 @Global()
 @Module({
-  providers: [dbProvider, pubsubProvider, streamsAdapterProvider],
+  providers: [dbProvider, pubsubAdapterProvider, streamsAdapterProvider],
   exports: ['REDIS_CLIENT', 'REDIS_PUBSUB_ADAPTER', 'REDIS_STREAMS_ADAPTER'],
 })
 export default class RedisModule {}
